280017c78f255017ca605d26d89d859ec634ce55
maryam khodaparast <khcomputergirl@gmail.com>
2020-08-11 23:18:12 +0430

    Assignment solution

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,59 +1,39 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
-import Person from './Person/Person';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 import './App.css';
 
 class App extends Component {
   state = {
-    persons:[
-      {name:'Max',age:28},
-      {name:'Manu',age:29},
-      {name:'Maryam',age:25}
-    ]
+     username:'superMax'
   }
-  switchNameHandler = (newName) => {
-      //console.log('was Clicked!');
-      //Don't do this this.state.persons[0].name='Maximilian';
-      this.setState({
-        persons:[
-          {name:newName,age:28},
-          {name:'Manu',age:29},
-          {name:'Maryam',age:22}
-        ]
-      })
 
-  }
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons:[
-        {name:'Max',age:28},
-        {name:event.target.value,age:29},
-        {name:'Maryam',age:25}
-      ]
-    })
+  usernameChangedHandler = (event) => {//we use this syntax =() => {} and not (){} because if we want to use event handler (this) should refer to Class
+     this.setState({username:event.target.value});
   }
   render() {
-    const style = {
-      backgroundColor:'white',
-      font:'inherit',
-      border: '1px solid blue',
-      padding:'8px',
-      cursor:'pointer'
-    };
 
     return (
       <div className="App">
-        <h1>Hi, I'm a react app </h1>
-        <p>This is really working!</p>
-        <button 
-        style={style}
-        onClick={() => this.switchNameHandler('Maximilian!!')}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/> 
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} 
-        click={this.switchNameHandler.bind(this,'Max!')}
-        changed={this.nameChangedHandler}
-        >My Hobbies: racing</Person> 
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <ol>
+          <li>Create TWO new components: UserInput and UserOutput</li>
+          <li>UserInput should hold an input element, UserOutput two paragraphs</li>
+          <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
+          <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
+          <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
+          <li>Add a method to manipulate the state (=> an event-handler method)</li>
+          <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
+          <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
+          <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
+          <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
+        </ol>
+         <UserInput 
+         changed={this.usernameChangedHandler}
+         currentName={this.state.username}/>
+         <UserOutput userName={this.state.username}/>
+         <UserOutput userName={this.state.username}/>
+         <UserOutput userName="Max"/>
       </div>//this html code is jsx 
     );
     //Using the syntax bind is better than the syntax in line 31 but they are the same!
