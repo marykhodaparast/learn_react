83aea9f6d297756571e238eab7a7aa773b9a3c04
maryam khodaparast <khcomputergirl@gmail.com>
2020-08-11 21:00:13 +0430

    Adding two way bindings

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -23,6 +23,15 @@ class App extends Component {
       })
 
   }
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons:[
+        {name:'Max',age:28},
+        {name:event.target.value,age:29},
+        {name:'Maryam',age:25}
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -30,7 +39,10 @@ class App extends Component {
         <p>This is really working!</p>
         <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/> 
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this,'Max!')}>My Hobbies: racing</Person> 
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} 
+        click={this.switchNameHandler.bind(this,'Max!')}
+        changed={this.nameChangedHandler}
+        >My Hobbies: racing</Person> 
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>//this html code is jsx 
     );
