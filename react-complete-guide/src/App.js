20a58121bf0670763ce358756c78e349ac22d691
maryam khodaparast <khcomputergirl@gmail.com>
2020-08-11 15:26:14 +0430

    Passing method refrence between components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,45 +1,43 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 //import logo from './logo.svg';
 import Person from './Person/Person';
 import './App.css';
 
-const app = props =>{
-    const [ personState, setPersonsState] = useState({persons:[//array destructuring in ES6
+class App extends Component {
+  state = {
+    persons:[
       {name:'Max',age:28},
       {name:'Manu',age:29},
       {name:'Maryam',age:25}
-    ],
-    //otherState:'some other value'
-  });
-  const [otherState, setOtherState] = useState('some other vaue');
-  console.log(personState);
-  console.log(otherState);
-  const switchNameHandler = () => {
-     //console.log('was Clicked!');
+    ]
+  }
+  switchNameHandler = (newName) => {
+      //console.log('was Clicked!');
       //Don't do this this.state.persons[0].name='Maximilian';
-      setPersonsState({
+      this.setState({
         persons:[
-          {name:'Maximilian',age:28},
+          {name:newName,age:28},
           {name:'Manu',age:29},
           {name:'Maryam',age:22}
-        ],
-        otherState:personState.otherState
+        ]
       })
+
   }
+  render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app </h1>
         <p>This is really working!</p>
-        <button onClick={switchNameHandler}>Switch name</button>
-        <Person name={personState.persons[0].name} age={personState.persons[0].age}/> 
-        <Person name={personState.persons[1].name} age={personState.persons[1].age}>My Hobbies: racing</Person>
-        <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/> 
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this,'Max!')}>My Hobbies: racing</Person> 
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>//this html code is jsx 
     );
+    //Using the syntax bind is better than the syntax in line 31 but they are the same!
     //we can write <Person></Person> or <Person/> when we don't have anycode between <Person> and </Person>
     //return React.createElement('div',{className:'App'},null,React.createElement('h1',null,'Does this works now?!'));
-
+  }
 }
-export default app;
 
-
+export default App;
